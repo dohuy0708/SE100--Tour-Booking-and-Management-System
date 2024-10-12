5857f597f7063cfc18d86582d5086511af79c6d9
dohuy0708 <dohuy22314@gmail.com>
2024-10-12 16:18:16 +0700

    init frontend project

---
+++ frontend/src/App.js
@@ -0,0 +1,19 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Hello, H5-Banana team. Welcome you be here!.
+          <br />
+          Hope you have a good experience at this project.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
