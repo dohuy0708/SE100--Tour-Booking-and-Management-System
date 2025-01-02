09668c2f13022c3e8ea54ea15403bec832ec6cfc
dohuy0708 <dohuy22314@gmail.com>
2025-01-02 14:36:13 +0700

    userroute

--- refresh_backend/src/db/user.ts
+++ refresh_backend/src/db/user.ts
@@ -1,32 +1,35 @@
-import mongooser from 'mongoose';
+import mongooser from "mongoose";
 
-const UserSchema = new mongooser.Schema({  
-    user_name: {type: String, required: true},
-    email: {type: String, required: true},
-    phone_number:{type: String, required: true},
-    date_of_birth:{type: Date, required: true},
-    authentication:{
-        user_password: {type: String, required: true, select: false},
-        salt: {type: String, select: false},
-        sessionToken: {type: String, select: false},
-        verificationCode: { type: Number, select: false }, // Mã xác thực
-        resetCode: { type: Number, select: false }, // Mã reset mật khẩu
-        isVerified: { type: Boolean, require:true, default:false}, // Trạng thái xác minh
-    },
-   role:{type: String, required: true},
-    group_id:{type: mongooser.Schema.Types.ObjectId, ref: 'Group'},
+const UserSchema = new mongooser.Schema({
+  user_name: { type: String, required: true },
+  email: { type: String, required: true },
+  phone_number: { type: String, required: true },
+  date_of_birth: { type: Date, required: true },
+  authentication: {
+    user_password: { type: String, required: true, select: false },
+    salt: { type: String, select: false },
+    sessionToken: { type: String, select: false },
+    verificationCode: { type: Number, select: false }, // Mã xác thực
+    resetCode: { type: Number, select: false }, // Mã reset mật khẩu
+    isVerified: { type: Boolean, require: true, default: false }, // Trạng thái xác minh
+  },
+  role: { type: String, required: true },
+  group_id: { type: mongooser.Schema.Types.ObjectId, ref: "Group" },
 });
 
-export const UserModel = mongooser.model('User', UserSchema);
+export const UserModel = mongooser.model("User", UserSchema);
 
-
-export const getUsers=()=>UserModel.find();
-export const getUserByEmail=(email:string)=>UserModel.findOne({email});
-export const getUserBySessionToken=(sessionToken:string)=>UserModel.findOne({
-    'authentication.sessionToken':sessionToken,
-});
-export const getUserById=(user_id:string)=>UserModel.findById(user_id);
-export const createUser=(values: Record<string, any>)=> new UserModel(values)
-.save().then((user)=>user.toObject());
-export const deleteUserById=(user_id:string)=>UserModel.findByIdAndDelete({_id: user_id});
-export const updateUserById=(user_id:string, values: Record<string, any>)=> UserModel.findByIdAndUpdate(user_id, values);
+export const getUsers = () => UserModel.find();
+export const listUsers = (role: string) => UserModel.find({ role });
+export const getUserByEmail = (email: string) => UserModel.findOne({ email });
+export const getUserBySessionToken = (sessionToken: string) =>
+  UserModel.findOne({
+    "authentication.sessionToken": sessionToken,
+  });
+export const getUserById = (user_id: string) => UserModel.findById(user_id);
+export const createUser = (values: Record<string, any>) =>
+  new UserModel(values).save().then((user) => user.toObject());
+export const deleteUserById = (user_id: string) =>
+  UserModel.findByIdAndDelete({ _id: user_id });
+export const updateUserById = (user_id: string, values: Record<string, any>) =>
+  UserModel.findByIdAndUpdate(user_id, values);
--- refresh_backend/src/middleware/index.ts
+++ refresh_backend/src/middleware/index.ts
@@ -1,45 +1,52 @@
-import express from 'express';
-import {get, merge} from 'lodash';
-import {getUserBySessionToken} from '../db/user';
+import express from "express";
+import { get, merge } from "lodash";
+import { getUserBySessionToken } from "../db/user";
 
-export const isAuthenticated=async(req:express.Request, res:express.Response,next: express.NextFunction)=>{
-    try{
-        const sessionToken=req.cookies['5H-AUTH'];
-        if(!sessionToken){
-            res.status(401).json({message:'Chưa từng đăng nhập'}).end();
-            return;
-        }
-        const exitstingUser=await getUserBySessionToken(sessionToken);
-        if(!exitstingUser){
-            res.status(403).json({message:'Phiên đăng nhập không hợp lệ'}).end();
-            return
-        }
-        merge(req, {identity: exitstingUser});
-        next();
+export const isAuthenticated = async (
+  req: express.Request,
+  res: express.Response,
+  next: express.NextFunction
+) => {
+  try {
+    const sessionToken = req.cookies["5H-AUTH"];
+    if (!sessionToken) {
+      res.status(401).json({ message: "Chưa từng đăng nhập" }).end();
+      return;
     }
-    catch(error){
-        console.log("'Authentication error: ", error);
-        res.status(500).json({message:'Internal server error'}).end();
+    const exitstingUser = await getUserBySessionToken(sessionToken);
+    console.log(exitstingUser);
+    if (!exitstingUser) {
+      res.status(403).json({ message: "Phiên đăng nhập không hợp lệ" }).end();
+      return;
     }
-}
+    merge(req, { identity: exitstingUser });
+    next();
+  } catch (error) {
+    console.log("'Authentication error: ", error);
+    res.status(500).json({ message: "Internal server error" }).end();
+  }
+};
 
-export const isOwner=async(req:express.Request, res:express.Response, next: express.NextFunction)=>{
-    try{
-        const {id}=req.params;
-        const currentUserId=get(req, 'indentity._id') as string;
+export const isOwner = async (
+  req: express.Request,
+  res: express.Response,
+  next: express.NextFunction
+) => {
+  try {
+    const { id } = req.params;
+    const currentUserId = get(req, "indentity._id") as string;
 
-        if(!currentUserId){
-            res.status(403).json({message:'Chưa đăng nhập'}).end();
-            return;
-        }
-        if(currentUserId.toString()!==id){
-            res.status(403).json({message:'Không có quyền'}).end();
-            return;
-        }
-        next();
+    if (!currentUserId) {
+      res.status(403).json({ message: "Chưa đăng nhập" }).end();
+      return;
     }
-    catch(error){   
-        console.log(error);
-        res.status(400).json({message:'Lỗi'}).end();
+    if (currentUserId.toString() !== id) {
+      res.status(403).json({ message: "Không có quyền" }).end();
+      return;
     }
-}
+    next();
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({ message: "Lỗi" }).end();
+  }
+};
